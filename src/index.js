6435e8ae1c960c74754aafe4ad54aa29d19a39a7
vvsar <vvsar@mail.ru>
2020-10-21 18:09:26 +0300

    feat: mplement function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,35 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr) {  
+  let str = "";
+  let ddd = "";
+  let n = expr.length;
+  for (let i = 0; i < n; i++) {
+    let pair = expr.slice(i, i + 2);
+    let sign = "";
+    if (pair === "00") {
+      i++;
+      continue;  
+    } else if (pair === "10") {
+      sign = ".";
+      i++;
+    } else if (pair === "11") {
+      sign = "-";
+      i++;
+    } else {
+      str = str + " ";
+      i = i + 9;
+      continue;
+    }
+    ddd = ddd + sign;
+    if ((i + 1) % 10 !== 0) {
+      continue;
+    }
+    str = str + MORSE_TABLE[ddd];
+    ddd = "";
+  }
+  return str;
 }
 
 module.exports = {
